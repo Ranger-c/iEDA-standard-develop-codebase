c861c6155fc77b8a55369e7897280aa7db9c440a
Ranger-c <63995936+Ranger-c@users.noreply.github.com>
2021-08-09 17:53:05 +0800

    Update bubble_sort.cpp

--- algorithm/sort/bubble_sort/bubble_sort.cpp
+++ algorithm/sort/bubble_sort/bubble_sort.cpp
@@ -1,4 +1,12 @@
 /*
+ *1.无序数组经sort函数排序，传指针给array_mid_serch函数（即二分查找），还传入了n（数组长度），want_findd（目标元素，为了区分传值的概念，有别于want_find变量。
+ *2.利用数组长度n，我们取得刚开始数组的起始位置left和结束位置right，当left<=right时（也就是数组不为空时），进行如下算法
+ *（1）当arr[mid]=want_find时，找到则返回1
+ *（2）当arr[mid]<want_find时，说明目标元素在经由mid分割的右区间，故将区间起始位置left赋值为mid+1
+ *（3）当arr[mid]>want_find时，说明目标元素在经由mid分割的左区间，故将区间结束位置right赋值为mid-1
+ *3.若数组为空，即代表找不到，返回0
+ *4.用answer接收返回值，并用if语句判断返回值相应进行输出
+
  * Part of Cosmos by OpenGenus Foundation
  *
  * bubble sort synopsis
